efd2e5700b8fe12b3442e616db69bc37a5f0f0c4
Rakha HP <rpramudito@gmail.com>
2022-06-11 10:54:22 +0700

    Ini adalah solusi dari challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async function (emotion) {
+  let ixxRead = await promiseTheaterIXX();
+  let vgcRead = await promiseTheaterVGC();
+  try {
+    let hasil =
+      (await angryOrNot(ixxRead, emotion)) +
+      (await angryOrNot(vgcRead, emotion));
+    return hasil;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const angryOrNot = async function (data, emotion) {
+  let hasil = 0;
+  for (counter = 0; counter < data.length; counter++) {
+    if (data[counter].hasil == emotion) hasil++;
+  }
+  return hasil;
+};
 
 module.exports = {
   promiseOutput,
